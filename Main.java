6f07d1c8d6bd954d0415cb89891d2f16454668e2
Sheikh Nathaniei Cañada <90026678+snmc7@users.noreply.github.com>
2025-04-08 23:51:26 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+
+            Scanner myObj = new Scanner(System.in);
+
+
+            System.out.println("AI-driven Diet Tracker");
+
+
+            System.out.println("1- Start!");
+            System.out.println("0- Exit!");
+            int inita = myObj.nextInt();
+            switch (inita) {
+                case 1:
+                    System.out.println("HELLO");
+                    System.out.println("LEMME BE INTRUSIVE AND OBSERVE YOUR PALATE FOR YOUR BENEFIT TRUST");
+                    int scannerasde =1;
+                case 2:
+                    System.out.println("WHY WOULD YWOU EXIT");
+                    break;
+            }
+
+            break;
+
+
+
+        }
+    }
+}
